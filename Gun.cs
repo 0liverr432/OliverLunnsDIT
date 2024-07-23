182f3fc73777fe9f69eeae85c702548e0b11067d
Oliver <oliverlunn34@gmail.com>
2024-07-23 16:36:07 +1200

    Gun.cs

--- Gun.cs
+++ Gun.cs
@@ -1,43 +1,43 @@
 using System.Collections;
-using UnityEngine;
+using UnityEngine
 
 public class Gun : MonoBehaviour
 {
-    public float damage = 10f;
-    public float range = 100f;
-    public float fireRate = 0.5f; // Adjust the fire rate as needed
+    public float damage = 10f;    //the amount of damage the gun will do
+    public float range = 100f;    //the range the raycast will go to until 
+    public float fireRate = 0.5f; //how often the gun will fire
 
-    public Camera fpsCam;
-    public ParticleSystem muzzleFlash;
+    public Camera fpsCam;    //the main camera for the player
+    public ParticleSystem muzzleFlash;    //the particle effect for when the gun fires 
 
-    private bool canShoot = true;
+    private bool canShoot = true;    //used in order to set firerate
 
-    void Update()
+    void Update()    //this function is called every frame
     {
-        if (Input.GetButton("Fire1") && canShoot)
+        if (Input.GetButton("Fire1") && canShoot)    //finds when the left mouse button has been clicked 
         {
-            StartCoroutine(ShootWithDelay());
+            StartCoroutine(ShootWithDelay());    //calls the shoot function
         }
     }
-
-    IEnumerator ShootWithDelay()
+    
+    IEnumerator ShootWithDelay()    //sets the function and makes it so that firerate can be used 
     {
-        canShoot = false;
-        Shoot();
-        yield return new WaitForSeconds(1f / fireRate);
-        canShoot = true;
+        canShoot = false;    //so that gun firerate isnt infinite
+        Shoot(); // function fires a raycast to the middle of screen from gun barrel
+        yield return new WaitForSeconds(1f / fireRate);    //firerate
+        canShoot = true;    //now can be shot again
     }
 
-    void Shoot()
+    void Shoot()    //function to shoot
     {
-        muzzleFlash.Play();
+        muzzleFlash.Play();    //muzzle flash so you know when ou have fired 
 
-        RaycastHit hit;
-        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        RaycastHit hit;    //shoots a ray in the direction of the player
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))    //finds where to shoot and if target is in range
         {
-            Debug.Log(hit.transform.name);
+            Debug.Log(hit.transform.name);    //displays the name in the console; used for testing and debugging mostly
 
-            Target target = hit.transform.GetComponent<Target>();
+            Target target = hit.transform.GetComponent<Target>();    //takes health away from target
             if (target != null)
             {
                 target.TakeDamage(damage);
