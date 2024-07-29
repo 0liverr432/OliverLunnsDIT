2cfbca9e7c972b92abb737bf1dbab2ad328eef8d
Oliver <oliverlunn34@gmail.com>
2024-07-30 10:25:05 +1200

    Update MenuCameraMoving.cs

--- Scripts/Menu Scripts/MenuCameraMoving.cs
+++ Scripts/Menu Scripts/MenuCameraMoving.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-public class MenuCameramovement : MonoBehaviour
+public class OptionsMenu : MonoBehaviour
 {
     public Button optionsButton; // Reference to options button
     public Vector3 position;
@@ -14,14 +14,16 @@ public class MenuCameramovement : MonoBehaviour
     Vector3 currentVelocity;
     public float smoothTime = 0.5f;
     public GameObject Title;
+    public GameObject optionsMenu;
 
     void Start()
     {
         
-        endPosition = new Vector3(-5.28f, 7.53f, -54.4f);
+        endPosition = new Vector3(2.32f, 4f, 35.2f);
+        optionsMenu.SetActive(false);
         
         // Store the initial position of the MainCamera
-        position = new Vector3(-5.28f, 7.53f, 3.1f);
+        position = new Vector3(2.32f, 4f, 0.73f);
         MainCamera.transform.position = position;
         // Add listener to the options button
         if (optionsButton != null)
@@ -43,29 +45,41 @@ public class MenuCameramovement : MonoBehaviour
     void Update()
     {
         //check if the camera is moving and the og position is equal to the end position.
-        if (isMoving && position == endPosition)
+        if (isMoving = true && position != endPosition)
         {
+            while (isMoving == true)
+            {
             // Move the camera gradually
             MainCamera.transform.position = Vector3.SmoothDamp(transform.position, endPosition, ref currentVelocity, smoothTime);
             position = endPosition;
-
+            
+            
+            if (MainCamera.transform.position == endPosition && position == endPosition)
+            {
+            isMoving = false;
+            }
+            }
         }  
-    
-
     }
 
-    void MoveCameraSideways()
+    public void MoveCameraSideways()
     {   // Check if its being clicked
         Debug.Log("OptionsClicked");
-        position = new Vector3(-5.28f, 7.53f, -54.4f);
+        position = new Vector3(2.32f, 4f, 0.73f);
         // Start moving the camera sideways
         isMoving = true;
         Title.SetActive(false);
+        OptionsMenuActive();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         position = endPosition;
-        MainCamera.transform.position = new Vector3(-5.28f, 7.53f, -54.4f);
+        MainCamera.transform.position = new Vector3(2.32f, 4f, 0.73f);
+    }
+
+    public void OptionsMenuActive()
+    {
+        optionsMenu.SetActive(true);
     }
 }
